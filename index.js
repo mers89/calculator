cdd417184eb443bccab6246bdf7f7c483685575f
Mercy Manrique <mercymanrique@yahoo.com>
2017-09-27 16:44:53 -0700

    revised code

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 var box = document.getElementById('display');
+
 function addtoscreen(x){
-  box.value=x;
+  box.value+=x;
   if(x==='c'){
-    box.value=' ';
+    box.value='';
   }
 }
+
 function answer(){
-x=box.value
-x=eval(x);
-box.value=x;
+  x=box.value
+  x=eval(x);
+  box.value=x;
 }
 
 function power(){
-  x=box.value
+  x=box.value;
   x=eval(x*x);
   box.value=x;
 }
 
 function backspace(){
- var num = box.value;
- var len = num.length-1;
- var newNum = num.substring(0,len);
- box.value = newNum; 
-  
+  var num = box.value;
+  var len = num.length-1;
+  var newNum = num.substring(0,len);
+  box.value  = newNum;
 }
-
